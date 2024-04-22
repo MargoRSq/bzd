9ca5827a30127b56a8df1c557804f8cd534f5ade
margosq <slava1kvartovkin@gmail.com>
2024-04-22 09:04:16 +0300

    add mac os link

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-import webbrowser
-
 import uvicorn
 
 from src.app import app
 
-try:
-    uvicorn.run(app)
-finally:
-    webbrowser.open('http://127.0.0.1:8000/materials/materials')
+uvicorn.run(app)
